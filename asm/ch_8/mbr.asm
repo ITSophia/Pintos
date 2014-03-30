195394cd4ff1d94b2c2f2acdd15ae6801c401a83
newmlee <matthewlee0725@gmail.com>
2014-03-30 11:18:44 +0000

    更新

--- asm/ch_8/mbr.asm
+++ asm/ch_8/mbr.asm
@@ -1,141 +1,168 @@
-         app_lba_start equ 100
+; 原书对应程序：c08_mbr.asm
+; 文件说明：硬盘主引导扇区代码（加载程序）
+; 创建日期：2014-03-30
 
-SECTION mbr align=16 vstart=0x7c00
+; 声明常数（用户程序的起始逻辑扇区的区号），
+; 注意equ的作用就是一个代码替换的指令，
+; 有点C语言中宏定义的味道;
+; 另外，常数的申明不会占用汇编地址
+app_lba_start equ 100
 
-         mov ax,0
-         mov ss,ax
-         mov sp,ax
+; NASM汇编使用SECTION或者SEGMENT来定义段;
+; align是用来指定SEGMENT的汇编地址的对其方式的，
+; 因为Intel处理器要求段在内存中的起始物理地址起码是16字节对齐的，
+; 也就是说，必须是16的倍数，或者能被16整除;
+; vstart是用来使段内标号的汇编地址从该段的开头开始计算的，
+; 如果没有用vstart说明，
+; 则这个标号的汇编地址是从整个程序的开头开始计算的
+SECTION mbr align = 16 vstart = 0x7c00
+    ; 设置堆栈
+    mov ax, 0
+    mov ss, ax ; ss是stack segment，栈段寄存器
+    mov sp, ax ; sp是stack pointer，栈指针寄存器
 
-         mov ax,[cs:phy_base]
-         mov dx,[cs:phy_base+0x02]
-         mov bx,16
-         div bx
-         mov ds,ax
-         mov es,ax
+    ; 以下两条mov语句用于存放phy_base处的一个双字单元，
+    ; 因为双字是32位的，
+    ; 所以在16位的8086上，只能用两个寄存器存放，
+    ; 又因为Intel处理器是低端序列的，
+    ; 所以高16位存放在phy_base + 0x02处，
+    ; 低16位存放在phy_base处
+    mov ax, [cs:phy_base]
+    mov dx, [cs:phy_base + 0x02]
+    ; 一条div 16（右移4位）使得phy_base处的地址变为16位的段地址
+    mov bx, 16
+    div bx
+    mov ds, ax
+    mov es, ax
 
-         xor di,di
-         mov si,app_lba_start
-         xor bx,bx
-         call read_hard_disk_0
+    ; 以下开始读取程序的起始部分;
+    ; 首先用异或将di清零
+    xor di, di
+    ; 把程序在硬盘上的起始逻辑扇区号移入源索引寄存器si中
+    mov si, app_lba_start
+    ; 异或使bx清零
+    xor bx, bx
+    ; call调用read_hard_disk_0过程（函数），
+    ; 在这里做好看一下原书上第128页的图8-13，
+    ; 十分清楚地介绍了过程与过程调用
+    call read_hard_disk_0
 
-         mov dx,[2]
-         mov ax,[0]
-         mov bx,512
-         div bx
-         cmp dx,0
-         jnz @1
-         dec ax
-   @1:
-         cmp ax,0
-         jz direct
+    mov dx, [2]
+    mov ax, [0]
+    mov bx, 512
+    div bx
+    cmp dx, 0
+    jnz @1
+    dec ax
+    @1:
+        cmp ax,0
+        jz direct
 
+        push ds
 
-         push ds
+        mov cx,ax
+    @2:
+        mov ax,ds
+        add ax,0x20
+        mov ds,ax
 
-         mov cx,ax
-   @2:
-         mov ax,ds
-         add ax,0x20
-         mov ds,ax
+        xor bx,bx
+        inc si
+        call read_hard_disk_0
+        loop @2
 
-         xor bx,bx
-         inc si
-         call read_hard_disk_0
-         loop @2
+        pop ds
 
-         pop ds
+    direct:
+        mov dx,[0x08]
+        mov ax,[0x06]
+        call calc_segment_base
+        mov [0x06],ax
 
-   direct:
-         mov dx,[0x08]
-         mov ax,[0x06]
-         call calc_segment_base
-         mov [0x06],ax
+        mov cx,[0x0a]
+        mov bx,0x0c
 
-         mov cx,[0x0a]
-         mov bx,0x0c
+    realloc:
+        mov dx,[bx+0x02]
+        mov ax,[bx]
+        call calc_segment_base
+        mov [bx],ax
+        add bx,4
+        loop realloc
 
- realloc:
-         mov dx,[bx+0x02]
-         mov ax,[bx]
-         call calc_segment_base
-         mov [bx],ax
-         add bx,4
-         loop realloc
-
-         jmp far [0x04]
+        jmp far [0x04]
 
 ;-------------------------------------------------------------------------------
 read_hard_disk_0:
+    push ax
+    push bx
+    push cx
+    push dx
 
-         push ax
-         push bx
-         push cx
-         push dx
+    mov dx,0x1f2
+    mov al,1
+    out dx,al
 
-         mov dx,0x1f2
-         mov al,1
-         out dx,al
+    inc dx
+    mov ax,si
+    out dx,al
 
-         inc dx
-         mov ax,si
-         out dx,al
+    inc dx
+    mov al,ah
+    out dx,al
 
-         inc dx
-         mov al,ah
-         out dx,al
+    inc dx
+    mov ax,di
+    out dx,al
 
-         inc dx
-         mov ax,di
-         out dx,al
+    inc dx
+    mov al,0xe0
+    or al,ah
+    out dx,al
 
-         inc dx
-         mov al,0xe0
-         or al,ah
-         out dx,al
+    inc dx
+    mov al,0x20
+    out dx,al
 
-         inc dx
-         mov al,0x20
-         out dx,al
+    .waits:
+        in al,dx
+        and al,0x88
+        cmp al,0x08
+        jnz .waits
 
-  .waits:
-         in al,dx
-         and al,0x88
-         cmp al,0x08
-         jnz .waits
+        mov cx,256
+        mov dx,0x1f0
+    
+    .readw:
+        in ax,dx
+        mov [bx],ax
+        add bx,2
+        loop .readw
 
-         mov cx,256
-         mov dx,0x1f0
-  .readw:
-         in ax,dx
-         mov [bx],ax
-         add bx,2
-         loop .readw
+        pop dx
+        pop cx
+        pop bx
+        pop ax
 
-         pop dx
-         pop cx
-         pop bx
-         pop ax
-
-         ret
+        ret
 
 ;-------------------------------------------------------------------------------
 calc_segment_base:
+    push dx
 
-         push dx
+    add ax,[cs:phy_base]
+    adc dx,[cs:phy_base+0x02]
+    shr ax,4
+    ror dx,4
+    and dx,0xf000
+    or ax,dx
 
-         add ax,[cs:phy_base]
-         adc dx,[cs:phy_base+0x02]
-         shr ax,4
-         ror dx,4
-         and dx,0xf000
-         or ax,dx
+    pop dx
 
-         pop dx
-
-         ret
+    ret
 
 ;-------------------------------------------------------------------------------
-         phy_base dd 0x10000
+phy_base dd 0x10000
 
- times 510-($-$$) db 0
-                  db 0x55,0xaa
+times 510-($-$$) db 0
+                 db 0x55,0xaa
--- asm/ch_8/user.asm
+++ asm/ch_8/user.asm
@@ -18,177 +18,171 @@ SECTION header vstart=0
 ;===============================================================================
 SECTION code_1 align=16 vstart=0
 put_string:
+    mov cl,[bx]
+    or cl,cl
+    jz .exit
+    call put_char
+    inc bx
+    jmp put_string
 
-         mov cl,[bx]
-         or cl,cl
-         jz .exit
-         call put_char
-         inc bx
-         jmp put_string
-
-   .exit:
-         ret
+    .exit:
+        ret
 
 ;-------------------------------------------------------------------------------
 put_char:
+    push ax
+    push bx
+    push cx
+    push dx
+    push ds
+    push es
 
-         push ax
-         push bx
-         push cx
-         push dx
-         push ds
-         push es
+    mov dx,0x3d4
+    mov al,0x0e
+    out dx,al
+    mov dx,0x3d5
+    in al,dx
+    mov ah,al
 
-         mov dx,0x3d4
-         mov al,0x0e
-         out dx,al
-         mov dx,0x3d5
-         in al,dx
-         mov ah,al
+    mov dx,0x3d4
+    mov al,0x0f
+    out dx,al
+    mov dx,0x3d5
+    in al,dx
+    mov bx,ax
 
-         mov dx,0x3d4
-         mov al,0x0f
-         out dx,al
-         mov dx,0x3d5
-         in al,dx
-         mov bx,ax
+    cmp cl,0x0d
+    jnz .put_0a
+    mov ax,bx
+    mov bl,80
+    div bl
+    mul bl
+    mov bx,ax
+    jmp .set_cursor
 
-         cmp cl,0x0d
-         jnz .put_0a
-         mov ax,bx
-         mov bl,80
-         div bl
-         mul bl
-         mov bx,ax
-         jmp .set_cursor
+    .put_0a:
+        cmp cl,0x0a
+        jnz .put_other
+        add bx,80
+        jmp .roll_screen
 
- .put_0a:
-         cmp cl,0x0a
-         jnz .put_other
-         add bx,80
-         jmp .roll_screen
+    .put_other:
+        mov ax,0xb800
+        mov es,ax
+        shl bx,1
+        mov [es:bx],cl
 
- .put_other:
-         mov ax,0xb800
-         mov es,ax
-         shl bx,1
-         mov [es:bx],cl
+        shr bx,1
+        add bx,1
 
-         shr bx,1
-         add bx,1
+     .roll_screen:
+        cmp bx,2000
+        jl .set_cursor
 
- .roll_screen:
-         cmp bx,2000
-         jl .set_cursor
+        mov ax,0xb800
+        mov ds,ax
+        mov es,ax
+        cld
+        mov si,0xa0
+        mov di,0x00
+        mov cx,1920
+        rep movsw
+        mov bx,3840
+        mov cx,80
+    .cls:
+        mov word[es:bx],0x0720
+        add bx,2
+        loop .cls
 
-         mov ax,0xb800
-         mov ds,ax
-         mov es,ax
-         cld
-         mov si,0xa0
-         mov di,0x00
-         mov cx,1920
-         rep movsw
-         mov bx,3840
-         mov cx,80
- .cls:
-         mov word[es:bx],0x0720
-         add bx,2
-         loop .cls
+        mov bx,1920
 
-         mov bx,1920
+    .set_cursor:
+        mov dx,0x3d4
+        mov al,0x0e
+        out dx,al
+        mov dx,0x3d5
+        mov al,bh
+        out dx,al
+        mov dx,0x3d4
+        mov al,0x0f
+        out dx,al
+        mov dx,0x3d5
+        mov al,bl
+        out dx,al
 
- .set_cursor:
-         mov dx,0x3d4
-         mov al,0x0e
-         out dx,al
-         mov dx,0x3d5
-         mov al,bh
-         out dx,al
-         mov dx,0x3d4
-         mov al,0x0f
-         out dx,al
-         mov dx,0x3d5
-         mov al,bl
-         out dx,al
+        pop es
+        pop ds
+        pop dx
+        pop cx
+        pop bx
+        pop ax
 
-         pop es
-         pop ds
-         pop dx
-         pop cx
-         pop bx
-         pop ax
-
-         ret
+        ret
 
 ;-------------------------------------------------------------------------------
-  start:
-         mov ax,[stack_segment]
-         mov ss,ax
-         mov sp,stack_end
+start:
+    mov ax,[stack_segment]
+    mov ss,ax
+    mov sp,stack_end
 
-         mov ax,[data_1_segment]
-         mov ds,ax
+    mov ax,[data_1_segment]
+    mov ds,ax
 
-         mov bx,msg0
-         call put_string
+    mov bx,msg0
+    call put_string
 
-         push word [es:code_2_segment]
-         mov ax,begin
-         push ax
+    push word [es:code_2_segment]
+    mov ax,begin
+    push ax
 
-         retf
+    retf
 
-  continue:
-         mov ax,[es:data_2_segment]
-         mov ds,ax
+    continue:
+    mov ax,[es:data_2_segment]
+    mov ds,ax
 
-         mov bx,msg1
-         call put_string
+    mov bx,msg1
+    call put_string
 
-         jmp $
+    jmp $
 
 ;===============================================================================
 SECTION code_2 align=16 vstart=0
+begin:
+    push word [es:code_1_segment]
+    mov ax,continue
+    push ax
 
-  begin:
-         push word [es:code_1_segment]
-         mov ax,continue
-         push ax
-
-         retf
+    retf
 ;===============================================================================
 SECTION data_1 align=16 vstart=0
-
-    msg0 db '  This is NASM - the famous Netwide Assembler. '
-         db 'Back at SourceForge and in intensive development! '
-         db 'Get the current versions from http://www.nasm.us/.'
-         db 0x0d,0x0a,0x0d,0x0a
-         db '  Example code for calculate 1+2+...+1000:',0x0d,0x0a,0x0d,0x0a
-         db '     xor dx,dx',0x0d,0x0a
-         db '     xor ax,ax',0x0d,0x0a
-         db '     xor cx,cx',0x0d,0x0a
-         db '  @@:',0x0d,0x0a
-         db '     inc cx',0x0d,0x0a
-         db '     add ax,cx',0x0d,0x0a
-         db '     adc dx,0',0x0d,0x0a
-         db '     inc cx',0x0d,0x0a
-         db '     cmp cx,1000',0x0d,0x0a
-         db '     jle @@',0x0d,0x0a
-         db '     ... ...(Some other codes)',0x0d,0x0a,0x0d,0x0a
-         db 0
+msg0 db '  This is NASM - the famous Netwide Assembler. '
+     db 'Back at SourceForge and in intensive development! '
+     db 'Get the current versions from http://www.nasm.us/.'
+     db 0x0d,0x0a,0x0d,0x0a
+     db '  Example code for calculate 1+2+...+1000:',0x0d,0x0a,0x0d,0x0a
+     db '     xor dx,dx',0x0d,0x0a
+     db '     xor ax,ax',0x0d,0x0a
+     db '     xor cx,cx',0x0d,0x0a
+     db '  @@:',0x0d,0x0a
+     db '     inc cx',0x0d,0x0a
+     db '     add ax,cx',0x0d,0x0a
+     db '     adc dx,0',0x0d,0x0a
+     db '     inc cx',0x0d,0x0a
+     db '     cmp cx,1000',0x0d,0x0a
+     db '     jle @@',0x0d,0x0a
+     db '     ... ...(Some other codes)',0x0d,0x0a,0x0d,0x0a
+     db 0
 
 ;===============================================================================
 SECTION data_2 align=16 vstart=0
-
-    msg1 db '  The above contents is written by LeeChung. '
-         db '2011-05-06'
-         db 0
+msg1 db '  The above contents is written by LeeChung. '
+     db '2011-05-06'
+     db 0
 
 ;===============================================================================
 SECTION stack align=16 vstart=0
-
-         resb 256
+    resb 256
 
 stack_end:
 
