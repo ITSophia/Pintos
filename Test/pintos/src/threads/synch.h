49ec457f72328dbc6442674ebcd0761f154e512f
newmlee <matthewlee0725@gmail.com>
2014-06-17 22:01:09 +0000

    更新

--- Test/pintos/src/threads/synch.h
+++ Test/pintos/src/threads/synch.h
@@ -5,11 +5,11 @@
 #include <stdbool.h>
 
 /* A counting semaphore. */
-struct semaphore 
-  {
+struct semaphore
+{
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
-  };
+};
 
 void sema_init (struct semaphore *, unsigned value);
 void sema_down (struct semaphore *);
@@ -18,11 +18,11 @@ void sema_up (struct semaphore *);
 void sema_self_test (void);
 
 /* Lock. */
-struct lock 
-  {
+struct lock
+{
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-  };
+};
 
 void lock_init (struct lock *);
 void lock_acquire (struct lock *);
@@ -31,21 +31,31 @@ void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
 
 /* Condition variable. */
-struct condition 
-  {
+struct condition
+{
     struct list waiters;        /* List of waiting threads. */
-  };
+};
 
 void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
-/* Optimization barrier.
+/*
+ * 定义一个比较线程优先级高低的函数，
+ * 用于semaphore -> waiter队列的有序插入
+ */
+bool priority_cmp(
+    const struct list_elem *a,
+    const struct list_elem *b,
+    void *aux
+);
 
-   The compiler will not reorder operations across an
-   optimization barrier.  See "Optimization Barriers" in the
-   reference guide for more information.*/
+/* Optimization barrier.
+ *
+ *   The compiler will not reorder operations across an
+ *   optimization barrier.  See "Optimization Barriers" in the
+ *   reference guide for more information.*/
 #define barrier() asm volatile ("" : : : "memory")
 
 #endif /* threads/synch.h */
