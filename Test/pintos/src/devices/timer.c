bc0cd46c82c6bf7710ac82af1cddc179055828f9
newmlee <matthewlee0725@gmail.com>
2014-08-03 16:14:50 +0000

    修改timer.c和timer.h

--- Test/pintos/src/devices/timer.c
+++ Test/pintos/src/devices/timer.c
@@ -7,7 +7,7 @@
 #include "threads/interrupt.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
-  
+
 /* See [8254] for hardware details of the 8254 timer chip. */
 
 #if TIMER_FREQ < 19
@@ -17,8 +17,14 @@
 #error TIMER_FREQ <= 1000 recommended
 #endif
 
+/* 定义更新的时钟周期 */
+#define UPDATE_FREQ 4
+
 /* Number of timer ticks since OS booted. */
 static int64_t ticks;
+
+/* 定义一个sleep_list链表，用于存储休眠的进程 */
+static struct list sleep_list;
 
 /* Number of loops per timer tick.
    Initialized by timer_calibrate(). */
@@ -33,15 +39,17 @@ static void real_time_delay (int64_t num, int32_t denom);
 /* Sets up the timer to interrupt TIMER_FREQ times per second,
    and registers the corresponding interrupt. */
 void
-timer_init (void) 
+timer_init (void)
 {
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+  /* 初始化sleep_list链表 */
+  list_init(&sleep_list);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
 void
-timer_calibrate (void) 
+timer_calibrate (void)
 {
   unsigned high_bit, test_bit;
 
@@ -51,7 +59,7 @@ timer_calibrate (void)
   /* Approximate loops_per_tick as the largest power-of-two
      still less than one timer tick. */
   loops_per_tick = 1u << 10;
-  while (!too_many_loops (loops_per_tick << 1)) 
+  while (!too_many_loops (loops_per_tick << 1))
     {
       loops_per_tick <<= 1;
       ASSERT (loops_per_tick != 0);
@@ -68,7 +76,7 @@ timer_calibrate (void)
 
 /* Returns the number of timer ticks since the OS booted. */
 int64_t
-timer_ticks (void) 
+timer_ticks (void)
 {
   enum intr_level old_level = intr_disable ();
   int64_t t = ticks;
@@ -79,27 +87,15 @@ timer_ticks (void)
 /* Returns the number of timer ticks elapsed since THEN, which
    should be a value once returned by timer_ticks(). */
 int64_t
-timer_elapsed (int64_t then) 
+timer_elapsed (int64_t then)
 {
   return timer_ticks () - then;
-}
-
-/* Sleeps for approximately TICKS timer ticks.  Interrupts must
-   be turned on. */
-void
-timer_sleep (int64_t ticks) 
-{
-  int64_t start = timer_ticks ();
-
-  ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
    turned on. */
 void
-timer_msleep (int64_t ms) 
+timer_msleep (int64_t ms)
 {
   real_time_sleep (ms, 1000);
 }
@@ -107,7 +103,7 @@ timer_msleep (int64_t ms)
 /* Sleeps for approximately US microseconds.  Interrupts must be
    turned on. */
 void
-timer_usleep (int64_t us) 
+timer_usleep (int64_t us)
 {
   real_time_sleep (us, 1000 * 1000);
 }
@@ -115,7 +111,7 @@ timer_usleep (int64_t us)
 /* Sleeps for approximately NS nanoseconds.  Interrupts must be
    turned on. */
 void
-timer_nsleep (int64_t ns) 
+timer_nsleep (int64_t ns)
 {
   real_time_sleep (ns, 1000 * 1000 * 1000);
 }
@@ -128,7 +124,7 @@ timer_nsleep (int64_t ns)
    will cause timer ticks to be lost.  Thus, use timer_msleep()
    instead if interrupts are enabled. */
 void
-timer_mdelay (int64_t ms) 
+timer_mdelay (int64_t ms)
 {
   real_time_delay (ms, 1000);
 }
@@ -141,7 +137,7 @@ timer_mdelay (int64_t ms)
    will cause timer ticks to be lost.  Thus, use timer_usleep()
    instead if interrupts are enabled. */
 void
-timer_udelay (int64_t us) 
+timer_udelay (int64_t us)
 {
   real_time_delay (us, 1000 * 1000);
 }
@@ -154,30 +150,23 @@ timer_udelay (int64_t us)
    will cause timer ticks to be lost.  Thus, use timer_nsleep()
    instead if interrupts are enabled.*/
 void
-timer_ndelay (int64_t ns) 
+timer_ndelay (int64_t ns)
 {
   real_time_delay (ns, 1000 * 1000 * 1000);
 }
 
 /* Prints timer statistics. */
 void
-timer_print_stats (void) 
+timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
-/* Timer interrupt handler. */
-static void
-timer_interrupt (struct intr_frame *args UNUSED)
-{
-  ticks++;
-  thread_tick ();
-}
+
 
 /* Returns true if LOOPS iterations waits for more than one timer
    tick, otherwise false. */
 static bool
-too_many_loops (unsigned loops) 
+too_many_loops (unsigned loops)
 {
   /* Wait for a timer tick. */
   int64_t start = ticks;
@@ -201,7 +190,7 @@ too_many_loops (unsigned loops)
    differently in different places the results would be difficult
    to predict. */
 static void NO_INLINE
-busy_wait (int64_t loops) 
+busy_wait (int64_t loops)
 {
   while (loops-- > 0)
     barrier ();
@@ -209,12 +198,12 @@ busy_wait (int64_t loops)
 
 /* Sleep for approximately NUM/DENOM seconds. */
 static void
-real_time_sleep (int64_t num, int32_t denom) 
+real_time_sleep (int64_t num, int32_t denom)
 {
   /* Convert NUM/DENOM seconds into timer ticks, rounding down.
-          
-        (NUM / DENOM) s          
-     ---------------------- = NUM * TIMER_FREQ / DENOM ticks. 
+
+        (NUM / DENOM) s
+     ---------------------- = NUM * TIMER_FREQ / DENOM ticks.
      1 s / TIMER_FREQ ticks
   */
   int64_t ticks = num * TIMER_FREQ / denom;
@@ -224,14 +213,14 @@ real_time_sleep (int64_t num, int32_t denom)
     {
       /* We're waiting for at least one full timer tick.  Use
          timer_sleep() because it will yield the CPU to other
-         processes. */                
-      timer_sleep (ticks); 
+         processes. */
+      timer_sleep (ticks);
     }
-  else 
+  else
     {
       /* Otherwise, use a busy-wait loop for more accurate
          sub-tick timing. */
-      real_time_delay (num, denom); 
+      real_time_delay (num, denom);
     }
 }
 
@@ -242,5 +231,109 @@ real_time_delay (int64_t num, int32_t denom)
   /* Scale the numerator and denominator down by 1000 to avoid
      the possibility of overflow. */
   ASSERT (denom % 1000 == 0);
-  busy_wait (loops_per_tick * num / 1000 * TIMER_FREQ / (denom / 1000)); 
+  busy_wait (loops_per_tick * num / 1000 * TIMER_FREQ / (denom / 1000));
+}
+
+/* 自定义一个list_less_func *函数，用于插入排序 */
+bool ticks_cmp(
+    const struct list_elem *a,
+    const struct list_elem *b,
+    void *aux UNUSED
+) {
+    struct thread *ta = list_entry(a, struct thread, elem);
+    struct thread *tb = list_entry(b, struct thread, elem);
+
+    /* 在这里使用升序 */
+    if ((ta -> ticks) > (tb -> ticks)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/* Sleeps for approximately TICKS timer ticks.  Interrupts must
+   be turned on. */
+void timer_sleep(int64_t ticks) {
+    /* 定义一个thread变量，用于捕获当前线程 */
+    struct thread *get_current_thread;
+    enum intr_level old_level;
+
+    ASSERT (intr_get_level () == INTR_ON);
+
+    /*
+     * 注释掉原有的代码：
+     * while (timer_elapsed (start) < ticks)
+     *   thread_yield ();
+     */
+
+    if (ticks > 0) {
+        old_level = intr_disable();
+        get_current_thread = thread_current();
+        /* 设置当前线程真正需要被唤醒的时钟点 */
+        get_current_thread -> ticks = ticks + timer_ticks();
+
+        /* 排序插入 */
+        list_insert_ordered(
+            &sleep_list,
+            &get_current_thread -> elem,
+            (list_less_func *)&ticks_cmp,
+            NULL
+        );
+        /* 阻塞当前线程 */
+        thread_block();
+        intr_set_level(old_level);
+    }
+}
+
+/* Timer interrupt handler. */
+static void timer_interrupt(struct intr_frame *args UNUSED) {
+    struct list_elem *temp_elem;
+    /* ticks是从OS启动开始就一直存在的，在这里时其自加 */
+    ticks++;
+    /* Called by the timer interrupt handler at each timer tick.
+    Thus, this function runs in an external interrupt context. */
+    thread_tick ();
+
+    /* 如果是MLFQS情况下 */
+    if (thread_mlfqs) {
+        recent_cpu_increment();
+        if (ticks % TIMER_FREQ == 0) {
+            calculate_load_avg();
+            update_recent_cpu_and_load_avg();
+        }
+        if (ticks % UPDATE_FREQ == 0) {
+            calculate_mlfqs_priority(thread_current());
+        }
+    }
+
+    /*
+    * 接下来我们可以开始自定义了；
+    * 在这里我们理解为，用于计算ticks的进程（或线程）是一直都存在的，
+    * 所以不需要我们重新开启一个线程去统计时钟，直接在这里计算就可以了
+    *
+    * 那么好，我们之前定义了一个sleep_list，
+    * 这个sleep_list是用来存储当前的睡眠线程的，
+    * 我们在sleep_list中遍历，
+    * 每满足一个线程唤醒的条件（因为可能有多个线程会在同一时间内被唤醒，所以我们使用while()循环），
+    * 我们就将其唤醒
+    */
+
+    /* 也可以调用list_min()函数每次找出最小的那个，这样就不用排序了 */
+    /*
+     * 注意main()为什么要最后一个唤醒呢？
+     * 一个可能的解释是，主线程要等待子线程的运算结果，
+     * 所以在所有的运算结果可达之前，主线程不能先于子线程被唤醒
+     */
+    temp_elem = list_begin(&sleep_list);
+    while (temp_elem != list_end(&sleep_list)) {
+        struct thread *t = list_entry(temp_elem, struct thread, elem);
+        if (ticks < t -> ticks) {
+            break;
+        }
+        list_remove(temp_elem);
+        thread_unblock(t);
+        temp_elem = list_begin(&sleep_list);
+    }
+
+    test_yield();
 }
